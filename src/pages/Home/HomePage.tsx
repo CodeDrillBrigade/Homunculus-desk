7a9ecf39a683dd6f2f0fee1d2d936405436937b3
Giovanni <giovanni.f.liguori@gmail.com>
2024-03-19 21:17:18 +0100

    Commit

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,4 +1,4 @@
-import {Center, Grid, GridItem, Text} from "@chakra-ui/react";
+import {AbsoluteCenter, Grid, GridItem} from "@chakra-ui/react";
 import {useNavigate} from "react-router-dom";
 import "./home.css"
 import {DarkMode} from "../../components/ui/DarkMode";
@@ -13,11 +13,11 @@ export const HomePage = () =>
     return <>
         <Grid templateColumns='repeat(5, 1fr)' gap={6}>
             <GridItem w='100%' h={h} bg='blue.500' onClick={() => {goto(pages[0])}}>
-                <Center>
+                <AbsoluteCenter>
                     <p>
                         {pages[0].substring(0,1).toUpperCase().concat(pages[0].substring(1))}
                     </p>
-                </Center>
+                </AbsoluteCenter>
             </GridItem>
             <GridItem w='100%' h={h} bg='blue.500' onClick={() => {goto(pages[1])}}>
                 <p>
