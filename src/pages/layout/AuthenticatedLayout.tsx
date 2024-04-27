2752ab4df0d771de58255a67f6337e8b76469c0c
Vincenzo Pierro <vcp@icure.com>
2024-04-27 14:17:41 +0200

    :passport_control: Fixed refresh bug

--- src/pages/layout/AuthenticatedLayout.tsx
+++ src/pages/layout/AuthenticatedLayout.tsx
@@ -1,24 +1,23 @@
-import {Outlet, useLocation, useNavigate} from "react-router-dom";
-import {useAppDispatch, useAppSelector} from "../../hooks/redux";
-import {useEffect} from "react";
-import {jwtSelector} from "../../store/auth/auth-slice";
-import {getToken} from "../../store/auth/auth-thunk";
-import TopMenu from "../../components/ui/TopMenu";
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { useAppSelector } from '../../hooks/redux'
+import { useEffect } from 'react'
+import { jwtSelector } from '../../store/auth/auth-slice'
+import TopMenu from '../../components/ui/TopMenu'
 
 export const AuthenticatedLayout = () => {
-    const dispatch = useAppDispatch()
-    dispatch(getToken())
-    const jwt = useAppSelector(jwtSelector);
-    const navigate = useNavigate();
-    const { pathname } = useLocation()
+	const jwt = useAppSelector(jwtSelector)
+	const navigate = useNavigate()
+	const { pathname } = useLocation()
 
-    useEffect(() => {
-        if(!jwt && pathname !== "login") {
-            navigate("/login");
-        }
-    }, [jwt, navigate, pathname]);
-    return <>
-        <TopMenu />
-        <Outlet />
-    </>
+	useEffect(() => {
+		if (!jwt && pathname !== 'login') {
+			navigate('/login')
+		}
+	}, [jwt, navigate, pathname])
+	return (
+		<>
+			<TopMenu />
+			<Outlet />
+		</>
+	)
 }
--- src/store/auth/auth-middleware.ts
+++ src/store/auth/auth-middleware.ts
@@ -1,48 +1,50 @@
-import {
-    TypedStartListening,
-    createListenerMiddleware,
-} from "@reduxjs/toolkit";
-import { resetAuthenticationState, setAuthenticationState } from "./auth-slice";
-import { AppDispatch, RootState } from "..";
-import { getJwtExpirationMillis, isJwtInvalidOrExpired } from "../../utils/jwt-utils";
-import axios, { AxiosResponse } from "axios";
-import { JwtResponse } from "../../models/auth/JwtResponse";
-import { localStorageJwtKey, localStorageRefreshJwtKey } from "./auth-thunk";
+import { TypedStartListening, createListenerMiddleware } from '@reduxjs/toolkit'
+import { resetAuthenticationState, setAuthenticationState } from './auth-slice'
+import { AppDispatch, RootState } from '..'
+import { getJwtExpirationMillis, isJwtInvalidOrExpired } from '../../utils/jwt-utils'
+import axios, { AxiosResponse } from 'axios'
+import { JwtResponse } from '../../models/auth/JwtResponse'
+import { localStorageJwtKey, localStorageRefreshJwtKey } from './auth-thunk'
 
-export const authListenerMiddleware = createListenerMiddleware();
+export const authListenerMiddleware = createListenerMiddleware()
 
-type authListeningMiddlewareType = TypedStartListening<RootState, AppDispatch>;
+type authListeningMiddlewareType = TypedStartListening<RootState, AppDispatch>
 
-const startAuthListening =
-    authListenerMiddleware.startListening as authListeningMiddlewareType;
+const startAuthListening = authListenerMiddleware.startListening as authListeningMiddlewareType
 
 startAuthListening({
-    actionCreator: setAuthenticationState,
-    effect: async (action, listenerApi) => {
-        listenerApi.cancelActiveListeners();
+	actionCreator: setAuthenticationState,
+	effect: async (action, listenerApi) => {
+		listenerApi.cancelActiveListeners()
 
-        const { jwt, refreshJwt } = action.payload;
-        const expirationDelay = !!jwt ? (getJwtExpirationMillis(jwt) - new Date().getTime()) - 10000 : 0
-        await listenerApi.delay(expirationDelay);
+		const { jwt, refreshJwt } = action.payload
+		const expirationDelay = !!jwt ? getJwtExpirationMillis(jwt) - new Date().getTime() - 10000 : 0
+		await listenerApi.delay(expirationDelay)
 
-        if(!!refreshJwt && !isJwtInvalidOrExpired(refreshJwt)) {
-            await axios.post(
-                `${process.env.REACT_APP_APIURL}/auth/refresh`,
-                null,
-                { headers: { "Authentication": `Bearer ${refreshJwt}` } }
-            ).then(
-                (response: AxiosResponse<JwtResponse, any>) => {
-                    listenerApi.dispatch(setAuthenticationState({
-                        jwt: response.data.jwt,
-                        refreshJwt: null
-                    }))
-                },
-                () => {
-                    localStorage.removeItem(localStorageJwtKey);
-                    localStorage.removeItem(localStorageRefreshJwtKey);
-                    listenerApi.dispatch(resetAuthenticationState())
-                }
-            );
-        }
-    }
-});
+		if (!!refreshJwt && !isJwtInvalidOrExpired(refreshJwt)) {
+			await axios
+				.post(`${process.env.REACT_APP_APIURL}/auth/refresh`, null, {
+					headers: { Authorization: `Bearer ${refreshJwt}` },
+				})
+				.then(
+					(response: AxiosResponse<JwtResponse, any>) => {
+						listenerApi.dispatch(
+							setAuthenticationState({
+								jwt: response.data.jwt,
+								refreshJwt: null,
+							})
+						)
+					},
+					() => {
+						localStorage.removeItem(localStorageJwtKey)
+						localStorage.removeItem(localStorageRefreshJwtKey)
+						listenerApi.dispatch(resetAuthenticationState())
+					}
+				)
+		} else {
+			localStorage.removeItem(localStorageJwtKey)
+			localStorage.removeItem(localStorageRefreshJwtKey)
+			listenerApi.dispatch(resetAuthenticationState())
+		}
+	},
+})
