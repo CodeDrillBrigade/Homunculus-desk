0e6f0f17966e02167ad740da4781142ff2173445
Vincenzo Pierro <vcp@icure.com>
2024-05-25 11:51:49 +0200

    :bug: automatically refresh token in thunk

--- src/store/auth/auth-thunk.ts
+++ src/store/auth/auth-thunk.ts
@@ -1,6 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { AuthState, resetAuthenticationState, setAuthenticationState } from './auth-slice'
 import { userApi } from '../../services/user'
+import { isJwtInvalidOrExpired } from '../../utils/jwt-utils'
+import axios, { AxiosResponse } from 'axios'
+import { JwtResponse } from '../../models/auth/JwtResponse'
 
 export const localStorageJwtKey = 'jwt'
 export const localStorageRefreshJwtKey = 'refreshJwt'
@@ -13,11 +16,25 @@ export const getToken = createAsyncThunk('auth/token', async (_param: void, { ge
 	if (!jwt) {
 		const loadedJwt = localStorage.getItem(localStorageJwtKey)
 		const refreshJwt = localStorage.getItem(localStorageRefreshJwtKey)
-		const data = { jwt: loadedJwt, refreshJwt }
-		if (!!loadedJwt) {
+		if (!!refreshJwt && !isJwtInvalidOrExpired(refreshJwt)) {
+			const response: AxiosResponse<JwtResponse, any> = await axios.post(
+				`${process.env.REACT_APP_APIURL}/auth/refresh`,
+				null,
+				{
+					headers: { Authorization: `Bearer ${refreshJwt}` },
+				}
+			)
+			localStorage.setItem(localStorageJwtKey, response.data.jwt)
+			const data = { jwt: response.data.jwt, refreshJwt }
 			dispatch(setAuthenticationState(data))
+			return data.jwt
+		} else {
+			const data = { jwt: loadedJwt, refreshJwt }
+			if (!!loadedJwt) {
+				dispatch(setAuthenticationState(data))
+			}
+			return data.jwt
 		}
-		return data.jwt
 	} else {
 		return jwt
 	}
