5f69b95c1632fe3e0e6d3ee0a956a628209b0870
Vincenzo Pierro <vcp@icure.com>
2024-06-20 19:45:12 +0200

    :recycle: Added recent materials to selector

--- src/components/forms/controls/MaterialSelector.tsx
+++ src/components/forms/controls/MaterialSelector.tsx
@@ -1,4 +1,7 @@
 import {
+	Alert,
+	AlertIcon,
+	Center,
 	Flex,
 	FormControl,
 	FormLabel,
@@ -21,7 +24,7 @@ import { FormValue } from '../../../models/form/FormValue'
 import { FormControls, useFormControl } from '../../../hooks/form-control'
 import React, { useCallback, useEffect, useState } from 'react'
 import { Material } from '../../../models/Material'
-import { useFindMaterialsByFuzzyNameQuery } from '../../../services/material'
+import { useFindMaterialsByFuzzyNameQuery, useGetLastCreatedQuery } from '../../../services/material'
 import { generateSkeletons } from '../../ui/StackedSkeleton'
 import { ErrorAlert } from '../../errors/ErrorAlert'
 
@@ -48,13 +51,15 @@ export function MaterialSelector({
 	const innerSetValue = controls?.setValue ?? setValue
 	const [isTyping, setIsTyping] = useState(false)
 	const { isOpen, onOpen: popoverOpen, onClose: popoverClose } = useDisclosure()
-	const [inputValue, setInputValue] = useState(controls?.value?.value?.name ?? '')
+	const [inputValue, setInputValue] = useState<string | undefined>(controls?.value?.value?.name)
 	const [queryValue, setQueryValue] = useState('')
+	const { data: defaultValues } = useGetLastCreatedQuery(5)
 	const { data, error, isFetching } = useFindMaterialsByFuzzyNameQuery(
 		{ query: queryValue, limit: 10 },
 		{ skip: queryValue.length <= 0 }
 	)
 
+	const materials = data ?? defaultValues
 	const handleChange = useCallback(
 		(e: React.ChangeEvent<HTMLInputElement>) => {
 			if (!isOpen) {
@@ -72,36 +77,36 @@ export function MaterialSelector({
 
 	const handleSelection = useCallback(
 		(id: string | undefined) => {
-			const material = data?.find(it => it._id === id)
+			const material = materials?.find(it => it._id === id)
 			innerSetValue(material)
 			if (!!material) {
 				setInputValue(material.name)
 			}
 		},
-		[data, innerSetValue]
+		[materials, innerSetValue]
 	)
 
 	const onInputBlur = useCallback(() => {
-		const firstId = !!data && data.length > 0 ? data[0]._id : undefined
+		const firstId = !!materials && materials.length > 0 ? materials[0]._id : undefined
 		handleSelection(firstId)
 		popoverClose()
-	}, [data, handleSelection, popoverClose])
+	}, [materials, handleSelection, popoverClose])
 
 	const onTabPressed = useCallback(
 		(event: React.KeyboardEvent<HTMLInputElement>) => {
 			if (event.key === 'Tab') {
-				const firstId = !!data && data.length > 0 ? data[0]._id : undefined
+				const firstId = !!materials && materials.length > 0 ? materials[0]._id : undefined
 				handleSelection(firstId)
 				popoverClose()
 			}
 		},
-		[data, handleSelection, popoverClose]
+		[materials, handleSelection, popoverClose]
 	)
 
 	useEffect(() => {
 		setIsTyping(true)
 		const timeoutId = setTimeout(() => {
-			if (inputValue.trim().length > 0) {
+			if (!!inputValue && inputValue.trim().length > 0) {
 				setQueryValue(inputValue.trim())
 			}
 			setIsTyping(false)
@@ -142,9 +147,10 @@ export function MaterialSelector({
 				<PopoverContent width="100%">
 					<PopoverBody width="90vw">
 						<VStack align="flex-start">
-							{!!data &&
-								inputValue.length > 0 &&
-								data.map((it, idx) => (
+							{!!materials &&
+								materials.length > 0 &&
+								(!inputValue || inputValue.length > 0) &&
+								materials.map((it, idx) => (
 									<Flex key={it._id} justifyContent="flex-start" marginLeft="1em">
 										{idx === 0 && <Kbd marginRight="1em">Tab</Kbd>}
 										<Text
@@ -158,6 +164,14 @@ export function MaterialSelector({
 										</Text>
 									</Flex>
 								))}
+							{!!materials && materials.length === 0 && (
+								<Flex justifyContent="flex-start" marginLeft="1em" width="95%">
+									<Alert status="warning" borderRadius="md">
+										<AlertIcon />
+										There are no materials matching your search
+									</Alert>
+								</Flex>
+							)}
 							{isFetching && generateSkeletons({ quantity: 5, height: '1.5ex' })}
 							{!!error && <ErrorAlert info={{ label: 'Cannot load materials', reason: error }} />}
 						</VStack>
